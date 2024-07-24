636bb1aa1e59a3bae20fc77c4626554f41a3ef2d
Donavon Ellison <donavonellison@Donavons-MacBook-Pro.local>
2024-07-24 14:50:14 -0500

    Final polished version with extra things accounted for

--- app/src/main/java/com/adonyastudios/codingchallenge/DAG.java
+++ app/src/main/java/com/adonyastudios/codingchallenge/DAG.java
@@ -4,96 +4,95 @@ import java.util.ArrayList;
 
 public class DAG {
 
-//    public Vertex vertex;
-//    public ArrayList<Vertex> connectedVertices = new ArrayList<Vertex>();
-
-//    DAG(Vertex vertexImported){
-//        this.vertex = vertexImported;
-//    }
     private int longestPathStepCount = 0;
     private String pathing = "";
+    private String finalPathing="";
 
     public void connectVertices(Vertex startingVertex, Vertex pointToVertex){
         //edge case 1 : to prevent connecting to itself
         if (startingVertex.equals(pointToVertex)){
             System.err.println("Cannot connect to itself");
         } //edge case 2 : the vertex already is connected to
-        else if(startingVertex.connectedVertices.contains(pointToVertex)){
+        else if(startingVertex.toVertices.contains(pointToVertex)){
             System.err.println("Cannot have duplicate connections");
         } //edge case 3 :  the vertex that is attempting to be connected to already connects to this one + no backwards movement
-        else if(pointToVertex.connectedVertices.contains(startingVertex)){
+        else if(pointToVertex.toVertices.contains(startingVertex)){
             System.err.println("Cannot connect to previous Vertex");
         } //edge case 4 : preventing connecting a vector in the opposite direction
         else if(startingVertex.layer>pointToVertex.layer){
             System.err.println("Cannot connect to a Vertex on a higher level");
         }else{
-            if(!startingVertex.connectedVertices.isEmpty()) {
-
-                //edge case 5 : preventing connecting a vector connection in the opposite direction
-                for (int i = 0; i < startingVertex.connectedVertices.size(); i++) {
-                    if (startingVertex.connectedVertices.get(i).layer > pointToVertex.layer) {
-                        System.err.println("Cannot contain a Vertex that connects to a higher layer"+ startingVertex +" "+ pointToVertex);
-                        break;
-                    }
-                }
-            }
-            startingVertex.connectedVertices.add(pointToVertex);
+            startingVertex.toVertices.add(pointToVertex);
+            pointToVertex.fromVertices.add(startingVertex);
         }
     }
 
     public String calculateLongestPath(Vertex vertex){
         int totalSteps =0;
         // case 6 : Vertex doesn't have any other connections
-        if(vertex.connectedVertices.isEmpty()){
-            System.out.println( "Vertex: "+ vertex.name+" has no other connections making its longest path 1");
+        if(vertex.toVertices.isEmpty()){
+//            System.out.println( "Vertex: "+ vertex.name+" has no other connections making its longest path 0");
             return "Vertex: "+ vertex.name+" has no other connections making its longest path 1";
         }
 
+        // case 7 : Vertex has only 1 other connection
+        if(vertex.toVertices.size() == 1){
+            longestPathStepCount=1;
+            finalPathing = vertex.name + " -> " + vertex.toVertices.get(0).name;
+//            System.out.println("The longest path of Vertex "+ vertex.name+ " is \n"+
+//                    finalPathing + "\n with a total of "+longestPathStepCount+ " steps");
+            return "The longest path of Vertex "+ vertex.name+ " is \n"+
+                    finalPathing + "\n with a total of "+longestPathStepCount+ " steps";
+        }
         for (Vertex outsideVertex:
-             vertex.connectedVertices) {
+             vertex.toVertices) {
             int insideSteps =1;
-            System.out.println("From given vertex: "+vertex.name+ " stepped to: "+ outsideVertex.name);
-            if(pathing.isEmpty()){
-                pathing = vertex.name + " -> "+outsideVertex.name;
-            }
-            System.out.println("Starting on step: "+ insideSteps);
-            if (!outsideVertex.connectedVertices.isEmpty()){
-                System.out.println(outsideVertex.name+ " goes deeper. It connects to "+ outsideVertex.connectedVertices);
-                deeperComputation(outsideVertex,outsideVertex.connectedVertices,insideSteps);
+//            System.out.println("From given vertex: "+vertex.name+ " stepped to: "+ outsideVertex.name);
+
+            pathing = vertex.name + " -> "+outsideVertex.name;
+
+//            System.out.println("Starting on step: "+ insideSteps);
+
+            if (!outsideVertex.toVertices.isEmpty()){
+//                System.out.println(outsideVertex.name+ " goes deeper. It connects to "+ outsideVertex.toVertices);
+                deeperComputation(outsideVertex,outsideVertex.toVertices,insideSteps);
             }
         }
-        System.out.println("The longest path of Vertex "+ vertex.name+ " is \n"+
-                pathing);
-        return null;
+//        System.out.println("The longest path of Vertex "+ vertex.name+ " is \n"+
+//                finalPathing + "\n with a total of "+longestPathStepCount+ " steps");
+        return "The longest path of Vertex "+ vertex.name+ " is \n"+
+                finalPathing + "\n with a total of "+longestPathStepCount+ " steps";
     }
 
-    private ArrayList<Vertex> deeperComputation(Vertex vertex,ArrayList<Vertex> vertexArrayList, int stepCount){
+    private void deeperComputation(Vertex vertex,ArrayList<Vertex> vertexArrayList, int stepCount){
         int startingStepCount = stepCount;
         String startingPathing = pathing;
         for (Vertex insideVertex:vertexArrayList
              ) {
             stepCount=startingStepCount;
             pathing = startingPathing;
-            if(!insideVertex.connectedVertices.isEmpty()){
+            if(!insideVertex.toVertices.isEmpty()){
                 stepCount++;
+                pathing += " -> "+ insideVertex.name ;
+//                System.out.println("Updating Pathing: "+ pathing);
                 if(stepCount>longestPathStepCount){
-                    pathing += " -> "+ insideVertex.name ;
-                    System.out.println("Starting Pathing: "+ pathing);
+                    finalPathing = pathing ;
+//                    System.out.println("final Pathing updated: "+ finalPathing);
                     longestPathStepCount=stepCount;
                 }
-                System.out.println("Current vertex is: "+vertex.name+ " stepping to: "+ insideVertex.name+ " and on step: "+ stepCount);
-                deeperComputation(insideVertex, insideVertex.connectedVertices,stepCount);
+//                System.out.println("Current vertex is: "+vertex.name+ " stepping to: "+ insideVertex.name+ " and on step: "+ stepCount);
+                deeperComputation(insideVertex, insideVertex.toVertices,stepCount);
             } else{
                 stepCount++;
-                System.out.println("stepped from: "+ vertex+ " to: "+ insideVertex+" and ended the journey on step: "+ stepCount);
+                pathing += " -> "+ insideVertex.name ;
+//                System.out.println("stepped from: "+ vertex+ " to: "+ insideVertex+" and ended the journey on step: "+ stepCount);
                 if(stepCount>longestPathStepCount){
-                    pathing += " -> "+ insideVertex.name ;
-                    System.out.println("ending pathing: "+ pathing);
+                    finalPathing = pathing ;
+//                    System.out.println("Final final pathing: "+ finalPathing);
                     longestPathStepCount=stepCount;
                 }
             }
         }
-        return null;
     }
 
 }
--- app/src/main/java/com/adonyastudios/codingchallenge/Vertex.java
+++ app/src/main/java/com/adonyastudios/codingchallenge/Vertex.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Vertex {
     public String name;
     public int layer;
-    public ArrayList<Vertex> connectedVertices = new ArrayList<>();
+    public ArrayList<Vertex> toVertices = new ArrayList<>();
+    public ArrayList<Vertex> fromVertices = new ArrayList<>();
     Vertex(String nameImported,int layerImported){
         this.name = nameImported;
         this.layer = layerImported;
